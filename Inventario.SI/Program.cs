0e448c94b9cfab0700aac924c10bf671851d8ec5
jusMa13 <jusherramarenco@gmail.com>
2026-05-12 15:15:02 -0600

    aplicar la parte del cors para lograr que React funcione

--- Inventario.SI/Program.cs
+++ Inventario.SI/Program.cs
@@ -4,31 +4,39 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
-builder.Services.AddDbContext
-    <AppDbContext>(options =>
+
+builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services
-  .AddScoped
-  <GestorInventario>();
+builder.Services.AddScoped<GestorInventario>();
+
+//hoy hicimos este pedazo
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+
 }
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+app.UseCors("AllowReactApp"); 
 
 app.MapControllers();
 
